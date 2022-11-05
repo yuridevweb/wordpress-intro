f56d754ef240aa2b5ddff28be6c70157c262d88d
yuridevweb <yuridevweb@gmail.com>
2022-11-05 10:17:52 +0000

    Add aria label

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<div id="banner">
+<div id="banner" aria-label="Belgium fries">
     <div class="banner-title">
         <h1 class="primary-color">BELGIUM FRIES,</h1>
         <h1 class="secondary-color">YOUR WAY</h1>
@@ -11,6 +11,8 @@
 
 
 <main class="container">
+    <!-- Not sure how to implement background waves -->
+    
     <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave">
             <path fill="#030303" fill-opacity="1"
                 d="M0,96L80,128C160,160,320,224,480,256C640,288,800,288,960,266.7C1120,245,1280,203,1360,181.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
@@ -23,7 +25,7 @@
         <div class="button-wrapper"><button class="prev" type="button" onclick="prevSlide(this)">➤</button>
         </div>
 
-        <div class="slider" id="slider">
+        <div class="slider" id="slider" aria-label="Delicious menu">
             <div class="item">
                 <div class="item-bg">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/food-1.png" alt="lorem">

0206c4026ab85ca173ab651314b08f5812899a13
yuridevweb <yuridevweb@gmail.com>
2022-11-06 12:01:14 +0000

    Update function

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 
 // adding the CSS and JS files
 
-function gt_setup()
+function patat_setup()
 {   wp_enqueue_style('google-fonts','//fonts.googleapis.com/css2?family=Knewave&family=Roboto&display=swap');
     wp_enqueue_style('fontawesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css');
     wp_enqueue_style('style', get_stylesheet_uri(), NULL, microtime(), 'all');
     wp_enqueue_script('main', get_theme_file_uri('/js/main.js'), NULL, microtime(), true);
 }
 
-add_action('wp_enqueue_scripts', 'gt_setup');
+add_action('wp_enqueue_scripts', 'patat_setup');

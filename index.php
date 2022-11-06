1aaef35a195ae4395c3939f5643417dde4b5172d
yuridevweb <yuridevweb@gmail.com>
2022-11-06 12:00:55 +0000

    Slider refactoring

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <main class="container">
     <!-- Not sure how to implement background waves -->
-    
+
     <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave">
             <path fill="#030303" fill-opacity="1"
                 d="M0,96L80,128C160,160,320,224,480,256C640,288,800,288,960,266.7C1120,245,1280,203,1360,181.3L1440,160L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z">
@@ -21,39 +21,39 @@
     <h2>WHAT'S YOUR</h2>
     <h2 class="ml"><span class="secondary-color">FLAVOUR</span> COMBINATION?</h2>
 
-    <div class="wrapper">
-        <div class="button-wrapper"><button class="prev" type="button" onclick="prevSlide(this)">➤</button>
+    <div class="wrapper" data-carousel>
+        <div class="button-wrapper"><button class="prev" data-carousel-button="prev">➤</button>
         </div>
 
-        <div class="slider" id="slider" aria-label="Delicious menu">
-            <div class="item">
+        <ul class="slider" id="slider" aria-label="Delicious menu" data-slides>
+            <li class="item" data-carousel-item>
                 <div class="item-bg">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/food-1.png" alt="lorem">
-                    <div class="title">Lorem</div>
+                    <div class="item-title">Lorem</div>
                 </div>
-            </div>
+            </li>
 
-            <div class="item">
-                <div class="item-bg active-item">
+            <li class="item" data-active data-carousel-item>
+                <div class="item-bg <!--active-item-->">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/food-2.png" alt="lorem">
-                    <div class="info">
-                        <div class="title">
-                            <p>Ipsum</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
-            <div class="item">
+                    <div class="item-title">
+                        <p>Ipsum</p>
+                    </div>
+
+                </div>
+            </li>
+
+            <li class="item" data-carousel-item>
                 <div class="item-bg">
                     <img src="<?php echo get_template_directory_uri(); ?>/img/food-3.png" alt="lorem">
-                    <div class="title">Dolor sit amet</div>
+                    <div class="item-title">Dolor sit amet</div>
                 </div>
-            </div>
+            </li>
 
-        </div>
+        </ul>
         <div class="button-wrapper">
-            <button class="next" type="button" onclick="nextSlide(this)">➤</button>
+            <button class="next" data-carousel-button="next">➤</button>
         </div>
     </div>
 
